96689c059576ec36d3dd0d5e865b11baf2a6d876
Mike Testen <mrtesten@gmail.com>
2013-11-07 09:25:05 -0500

    Added map fragment.

--- ArcticThunder/src/com/missionse/arcticthunder/MainActivity.java
+++ ArcticThunder/src/com/missionse/arcticthunder/MainActivity.java
@@ -1,19 +1,32 @@
 package com.missionse.arcticthunder;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.missionse.arcticthunder.map.MapsFragment;
 
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		FragmentManager fm = getFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+		if (fragment == null) {
+			fragment = new MapsFragment();
+			fm.beginTransaction()
+				.add(R.id.fragmentContainer, fragment)
+				.commit();
+		}
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(final Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
---
+++ ArcticThunder/src/com/missionse/arcticthunder/map/MapsFragment.java
@@ -0,0 +1,136 @@
+package com.missionse.arcticthunder.map;
+
+import static com.google.android.gms.maps.GoogleMap.MAP_TYPE_HYBRID;
+import android.app.Fragment;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.missionse.arcticthunder.R;
+
+public class MapsFragment extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener,
+LocationListener, OnMyLocationButtonClickListener {
+
+	private static final LatLng MSE = new LatLng(39.974552, -74.976844);
+	private static final LatLng ZONE_A = new LatLng(39.974074, -74.977462);
+	private static final LatLng ZONE_B = new LatLng(39.975233, -74.977328);
+	private static final LatLng ZONE_C = new LatLng(39.975085, -74.976164);
+
+	private GoogleMap mMap;
+
+	private LocationClient mLocationClient;
+
+	private boolean mFirstLocationChange = true;
+
+	// These settings are the same as the settings for the map. They will in fact give you updates
+	// at the maximal rates currently possible.
+	private static final LocationRequest REQUEST = LocationRequest.create().setInterval(5000) // 5 seconds
+			.setFastestInterval(16) // 16ms = 60fps
+			.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+
+		View v = inflater.inflate(R.layout.fragment_map, container, false);
+		return v;
+	}
+
+	@Override
+	public boolean onMyLocationButtonClick() {
+		return false;
+	}
+
+	@Override
+	public void onLocationChanged(final Location arg0) {
+		if (mFirstLocationChange) {
+			mMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition(MSE, 17.5f, 0, 27)));
+			mFirstLocationChange = false;
+		}
+	}
+
+	@Override
+	public void onConnectionFailed(final ConnectionResult arg0) {
+		// Do nothing.
+	}
+
+	@Override
+	public void onConnected(final Bundle arg0) {
+		mLocationClient.requestLocationUpdates(REQUEST, this); // LocationListener
+	}
+
+	@Override
+	public void onDisconnected() {
+		// Do nothing.
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+		setUpLocationClientIfNeeded();
+		mLocationClient.connect();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		if (mLocationClient != null) {
+			mLocationClient.disconnect();
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(final MenuItem item) {
+		switch (item.getItemId()) {
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the map.
+		if (mMap == null) {
+			// Try to obtain the map from the SupportMapFragment.
+			mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
+
+	private void setUpLocationClientIfNeeded() {
+		if (mLocationClient == null) {
+			mLocationClient = new LocationClient(getActivity(), this, // ConnectionCallbacks
+					this); // OnConnectionFailedListener
+		}
+	}
+
+	private void setUpMap() {
+		mMap.setMyLocationEnabled(true);
+		mMap.setOnMyLocationButtonClickListener(this);
+		mMap.setBuildingsEnabled(true);
+		mMap.setMapType(MAP_TYPE_HYBRID);
+	}
+}
